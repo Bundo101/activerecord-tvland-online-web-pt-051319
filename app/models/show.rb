c4161634df4f9cc9ea8ab3c6a287579bcb027bbf
James Britton <brittonjp@gmail.com>
2019-08-05 15:42:56 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list
-    self.actors.map { |actor| "#{actor.first_name}"
+    self.actors.map { |actor| "#{actor.first_name} #{actor.last_name}" }
   end
     
 end
